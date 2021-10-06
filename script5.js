0870161994027a86d68745e90eb3043c1147955e
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2021-10-07 00:07:37 +0300

    one else commit

---
+++ script5.js
@@ -0,0 +1,17 @@
+
+var Objects = [];
+Objects[0] = {
+name: "first",
+age: 15 }
+Objects[1] = {
+name: "second",
+age: 22 }
+Objects[2] = {
+name: "third",
+age: 55}
+Objects[3] = {
+name: "firth",
+age: 99 };
+ $("#button1").click(function () {
+       alert ( Objects[0].name); }
+);
